5af69b8ad77c99ea2dd7addf08da973149348117
tonnysanjidaakter <tonnysanjidaakter@gmail.com>
2022-04-27 10:51:08 +0600

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import MyLineChart from './components/MyLineChart/MyLineChart';
+import SpecialChart from './components/SpecialChart/SpecialChart';
+import { useSpring, animated } from 'react-spring'
+import { useState } from 'react';
+
+
+
+
+
+function App(){
+  const [flip, set] = useState(false)
+  const props = useSpring({
+     to: { opacity: 1 }, 
+     from: { opacity: 0 },
+     reset: true,
+    reverse: flip,
+    delay: 200,
+    onRest: () => set(!flip),
+
+    })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <animated.div style={props}>I will fade in</animated.div>
+       <MyLineChart></MyLineChart>
+       <SpecialChart></SpecialChart>
     </div>
   );
 }
